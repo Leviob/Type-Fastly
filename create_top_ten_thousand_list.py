6cff135db5e538c86fc93db34ec39c885180ca6a
Levi Ob <commonnightingale@pm.me>
2021-05-23 23:11:46 -0700

    Make script more autonomous
    Functionality was changed such that 7 files are all created with one running of the script, rather than having to change the code for each run.

--- create_top_ten_thousand_list.py
+++ create_top_ten_thousand_list.py
@@ -7,14 +7,16 @@ import pprint
 frequency_list = FreqDist(i.lower() for i in brown.words())
 most_common = frequency_list.most_common()
 # print(len(brown.words())) # 1161192 total words
+print(brown.words()[1000:1075])
 # print(len(most_common)) # 49815 unique words.
-top_ten = []
+# top_ten = []
 
-for i in most_common:
-    if len(top_ten) < 10000:
-        word = i[0]
-        if word.isalpha() and len(word) > 4:
-            top_ten.append(word)
+# for i in range(7):
+#     for w in most_common:
+#         if len(top_ten) < 10000:
+#             word = w[0]
+#             if word.isalpha() and len(word) > i:
+#                 top_ten.append(word)
 
-fileObj = open('top_ten_5_or_longer.py', 'w')
-fileObj.write('top_ten = ' + pprint.pformat(top_ten) + '\n')
+#     fileObj = open(f'top_ten_{i + 1}_or_longer.py', 'w')
+#     fileObj.write('top_ten = ' + pprint.pformat(top_ten) + '\n')
