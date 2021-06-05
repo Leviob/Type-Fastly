66d37a8d5d6d6a70984e8d9bc004980239352349
Levi Ob <commonnightingale@pm.me>
2021-06-04 19:18:56 -0700

    Modify method to only include words in top 10000

--- create_top_ten_thousand_list.py
+++ create_top_ten_thousand_list.py
@@ -8,14 +8,15 @@ frequency_list = FreqDist(i.lower() for i in brown.words())
 most_common = frequency_list.most_common()
 # print(len(brown.words())) # 1161192 total words
 # print(len(most_common)) # 49815 unique words.
-top_ten = []
+# print(most_common[:20])
+
 
 for i in range(7):
-    for w in most_common:
-        if len(top_ten) < 10000:
-            word = w[0]
-            if word.isalpha() and len(word) > i:
-                top_ten.append(word)
+    top_ten = []
+    for word_index in range(10000):
+        word = most_common[word_index][0]
+        if word.isalpha() and len(word) > i:
+            top_ten.append(word)
 
     fileObj = open(f'top_ten_{i + 1}_or_longer.py', 'w')
-    fileObj.write('top_ten = ' + pprint.pformat(top_ten) + '\n')
+    fileObj.write(f'top_ten_{i + 1}_or_longer = ' + pprint.pformat(top_ten) + '\n')
